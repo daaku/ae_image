07816e11921f4eb6a347bf136c7ead7dbfd33f7a
Naitik Shah <n@daaku.org>
2011-05-22 23:43:45 -0400

    test for adding property on an existing model

--- app/tests/test_ae_image_db_property.py
+++ app/tests/test_ae_image_db_property.py
@@ -61,3 +61,23 @@ class CollectionPropertyTestCase(BaseTestCase):
             'Expect to generate some URLs.')
         self.assertTrue(
             album.images.get_url('big', blob_key), 'Expect URL back.')
+
+    def test_adding_property_to_existing_model(self):
+        class TestAlbumAlt(db.Model):
+            pass
+
+        album_key = 'test_adding_property_to_existing_model'
+        album = TestAlbumAlt(key_name=album_key)
+        album.save()
+
+        class TestAlbumAlt(db.Model):
+            images = ae_image.Property([
+                ae_image.Style('thumb', size=50, quality=75),
+                ae_image.Style('medium', size=300, quality=85)])
+
+        blob_key = self.make_blob('image/jpeg', 'dummy')
+        album = TestAlbumAlt.get_by_key_name(album_key)
+        album.images.append_from_blob_info(blobstore.BlobInfo.get(blob_key))
+        album.save()
+        self.assertTrue(
+            album.images.get_url('thumb', blob_key), 'Expect URL back.')
